a4d1c4406b817dd4bea64625cf406e55aaf2953d
Isabelle-Moura <mourabisabelle@gmail.com>
2024-05-07 01:41:26 -0300

    fix: App component. Router was missing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./assets/global-style/global-style";
 import { theme } from "./assets/theme/theme";
+import Router from "./routes/router";
 
 const App = () => {
    return (
       <>
          <ThemeProvider theme={theme}>
             <GlobalStyles />
-            <div>Aqui eh uma loucura loucura</div>
+            <Router />
          </ThemeProvider>
       </>
    );
