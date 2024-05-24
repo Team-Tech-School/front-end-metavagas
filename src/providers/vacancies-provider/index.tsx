46f4a5675b6ffc904cb8432a80f844311f4ee030
Isabelle-Moura <mourabisabelle@gmail.com>
2024-05-24 01:14:54 -0300

    feat: criar providers para auth e vacancies.

--- src/providers/vacancies-provider/index.tsx
+++ src/providers/vacancies-provider/index.tsx
@@ -1,20 +1,55 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-// import { api } from "../../api/api";
+import { api } from "../../api/api";
 
-// Aqui vão todas as funcionalidades, com as rotas dentro, relacionadas à entidade Vacancy.
+// Tipagem para as vagas
+interface Vacancy {
+   id: number;
+   title: string;
+   // Outros campos relevantes podem ser adicionados aqui
+}
 
 interface VacanciesProps {
    children: ReactNode;
 }
 
-interface VacanciesContextData {}
+interface VacanciesContextData {
+   vacancies: Vacancy[];
+   mostRecentVacancies: Vacancy[];
+   fetchMostRecentVacancies: () => Promise<void>;
+   fetchVacanciesForSelectedCategory: (search: string) => Promise<void>;
+}
 
 const VacanciesContext = createContext<VacanciesContextData>({} as VacanciesContextData);
 
 export const VacanciesProvider = ({ children }: VacanciesProps) => {
-   const [user, setUser] = useState({}); // Aqui é só para não ter um "value" vazio.
+   const [vacancies, setVacancies] = useState<Vacancy[]>([]);
+   const [mostRecentVacancies, setMostRecentVacancies] = useState<Vacancy[]>([]);
 
-   return <VacanciesContext.Provider value={user}>{children}</VacanciesContext.Provider>;
+   const fetchMostRecentVacancies = async () => {
+      try {
+         const response = await api.get(`/vacancy?limit=4`);
+         console.log(response.data.vacancies);
+         setMostRecentVacancies(response.data.vacancies);
+      } catch (error: any) {
+         console.error("Something went wrong at fetchVacancies: ", error);
+         setVacancies([]);
+         throw new Error(error);
+      }
+   };
+
+   const fetchVacanciesForSelectedCategory = async (categoryType: string) => {
+      try {
+         const response = await api.get(`/vacancy?limit=12&${categoryType}=`);
+         console.log(response.data.vacancies);
+         setVacancies(response.data.vacancies);
+      } catch (error: any) {
+         console.error("Something went wrong at fetchVacancies: ", error);
+         setVacancies([]);
+         throw new Error(error);
+      }
+   };
+
+   return <VacanciesContext.Provider value={{ vacancies, mostRecentVacancies, fetchMostRecentVacancies, fetchVacanciesForSelectedCategory }}>{children}</VacanciesContext.Provider>;
 };
 
 export const useVacanciesContext = () => useContext(VacanciesContext);
