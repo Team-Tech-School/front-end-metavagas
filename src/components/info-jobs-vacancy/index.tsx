69b7997b99079ad7af740dd673bb0a38fa999784
Isabelle-Moura <mourabisabelle@gmail.com>
2024-05-25 00:34:33 -0300

    chore: blur para usuário deslogado e infos aparecendo para usuário logado.

--- src/components/info-jobs-vacancy/index.tsx
+++ src/components/info-jobs-vacancy/index.tsx
@@ -4,48 +4,57 @@ import Cifrao from "../../assets/images/cifrao.png";
 import Local from "../../assets/images/local-mini.png";
 import Computer from "../../assets/images/computer-line.png";
 import { NewCard } from "../index";
+import { useAuthContext } from "../../providers/auth-provider";
 
 interface InfoJobsProps {
    page?: boolean;
    newVacancy?: boolean;
 }
 
-export const InfoJobs = ({page, newVacancy}: InfoJobsProps) => {
-  return (
-    <S.ResultInfoJobsDiv>
-      <S.Container>
-        
-        <S.ContainerTitle>
-           <div>
-           {newVacancy && <NewCard />}
-           </div>
-         <div>
-          <S.H1InfoJobs>Desenvolvedor de Sistemas Pleno</S.H1InfoJobs>
-          <S.SpanInfoJobs page={page}>Empresa: IZap Softworks</S.SpanInfoJobs>
-         </div>
-        </S.ContainerTitle>
-        <div>
-          <S.BoldText>
-            InfoJobs<S.StyledText> • 04 ago</S.StyledText>
-          </S.BoldText>
-        </div>
-      </S.Container>
-      <S.CardResultInfoJobs>
-        <RecentSearchButton title={"PHP"} />
-        <RecentSearchButton title={"Python"} />
-        <RecentSearchButton title={"React"} />
-      </S.CardResultInfoJobs>
-      <S.ContainerFooter>
-        <S.Container>
-            <S.StyledText page={page}> <img src={Cifrao} alt="cifrão" /> Faixa Salarial: <S.BoldText>R$ 4.000,00</S.BoldText></S.StyledText>
-            <S.StyledText page={page}> <img src={Local} alt="Localização" /> Localização: <S.BoldText>Belo Horizonte</S.BoldText></S.StyledText>
-            <S.StyledText page={page}> <img src={Computer} alt="Tipo de vaga" /> Tipo de vaga: <S.BoldText>Home office</S.BoldText></S.StyledText>
+export const InfoJobs = ({ page, newVacancy }: InfoJobsProps) => {
+   const { isLoggedIn } = useAuthContext();
+   const blurred = !isLoggedIn;
 
-        </S.Container>
-        <S.Paragraph>
-        Oferecemos um ambiente em que todos são protagonistas e agentes de transformação. Temos um excelente clima organizacional com pessoas engajadas em entregar os melhores resultados, assim como grandes oportunidades...
-        </S.Paragraph>
-      </S.ContainerFooter>
-    </S.ResultInfoJobsDiv>
-  );
+   return (
+      <S.ResultInfoJobsDiv newVacancy={newVacancy}>
+         <S.Container>
+            <S.ContainerTitle>
+               <div>{newVacancy && <NewCard />}</div>
+               <div>
+                  <S.H1InfoJobs>Desenvolvedor de Sistemas Pleno</S.H1InfoJobs>
+                  <S.SpanInfoJobs page={page} blurred={blurred}>
+                     Empresa: IZap Softworks
+                  </S.SpanInfoJobs>
+               </div>
+            </S.ContainerTitle>
+            <div>
+               <S.BoldText>
+                  InfoJobs<S.StyledText blurred={blurred}> • 04 ago</S.StyledText>
+               </S.BoldText>
+            </div>
+         </S.Container>
+         <S.CardResultInfoJobs>
+            <RecentSearchButton title={"PHP"} />
+            <RecentSearchButton title={"Python"} />
+            <RecentSearchButton title={"React"} />
+         </S.CardResultInfoJobs>
+         <S.ContainerFooter>
+            <S.Container>
+               <S.StyledText page={page} blurred={blurred}>
+                  <img src={Cifrao} alt="cifrão" /> Faixa Salarial: <S.BoldText>R$ 4.000,00</S.BoldText>
+               </S.StyledText>
+               <S.StyledText page={page} blurred={blurred}>
+                  <img src={Local} alt="Localização" /> Localização: <S.BoldText>Belo Horizonte</S.BoldText>
+               </S.StyledText>
+               <S.StyledText page={page} blurred={blurred}>
+                  <img src={Computer} alt="Tipo de vaga" /> Tipo de vaga: <S.BoldText>Home office</S.BoldText>
+               </S.StyledText>
+            </S.Container>
+            <S.Paragraph blurred={blurred}>
+               Oferecemos um ambiente em que todos são protagonistas e agentes de transformação. Temos um excelente clima organizacional com pessoas engajadas em entregar os melhores resultados, assim
+               como grandes oportunidades...
+            </S.Paragraph>
+         </S.ContainerFooter>
+      </S.ResultInfoJobsDiv>
+   );
 };
