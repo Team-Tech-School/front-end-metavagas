e38015d85e367f255aaef7d1082af2d12c1c2dc1
Isa Moura B. Bentes <mourabisabelle@gmail.com>
2024-05-30 23:15:14 -0300

    chore: organizando a main

--- src/components/number-vacancies/index.tsx
+++ src/components/number-vacancies/index.tsx
@@ -1,16 +1,18 @@
-import * as S from './style'
-import { SpanResult } from './style'
+import * as S from "./style";
+import { SpanResult } from "./style";
 
-interface Props {   
-    searchPlaceholder: string
-    vacanciesFound: number
+interface Props {
+   searchPlaceholder: string;
+   vacanciesFound: number;
 }
 
-export const NumberVacancies = ({searchPlaceholder,vacanciesFound}:Props) => {
-    return(
-        <>
-        <S.H1>Vagas em <S.SpanTitle>{searchPlaceholder}</S.SpanTitle></S.H1>
-        <SpanResult>{vacanciesFound} vagas encontradas</SpanResult>
-        </>
-    )
-}
+export const NumberVacancies = ({ searchPlaceholder, vacanciesFound }: Props) => {
+   return (
+      <>
+         <S.H1>
+            Vagas em <S.SpanTitle>{searchPlaceholder ? searchPlaceholder : "Tudo"}</S.SpanTitle>
+         </S.H1>
+         {vacanciesFound === 1 ? <SpanResult>{vacanciesFound} vaga encontrada</SpanResult> : <SpanResult>{vacanciesFound} vagas encontradas</SpanResult>}
+      </>
+   );
+};
