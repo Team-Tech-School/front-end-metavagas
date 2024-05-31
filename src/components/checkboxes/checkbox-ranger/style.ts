f26832d210f448c823fe8522906145dcb3375036
Isa Moura B. Bentes <mourabisabelle@gmail.com>
2024-05-30 23:02:28 -0300

    chore: organizando a main

--- src/components/checkboxes/checkbox-ranger/style.ts
+++ src/components/checkboxes/checkbox-ranger/style.ts
@@ -1,88 +1,46 @@
 import styled from "styled-components";
 
+interface CheckboxOptionProps {
+   checked: boolean;
+}
 
-export const SalaryRangeSliderContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const CheckboxFilterContainer = styled.div`
+   display: flex;
+   flex-direction: column;
+   margin-left: 10px;
 `;
 
-export const Label = styled.label`
-  width: 100%;
-  height: 24px;
-  margin-bottom: 5px;
-  font-family: 'Poppins', sans-serif;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 24px;
-  color: #1A1033;
+export const CheckboxLabel = styled.label`
+   margin-bottom: 0.5rem;
 `;
 
+export const CheckboxInput = styled.input`
+   margin-right: 0.2rem;
+   width: 15px; /* Tornar quadrado */
+   height: 15px; /* Tornar quadrado */
+   cursor: pointer; /* Cursor pointer para indicar que é clicável */
 
-export const Value = styled.span`
-  width: 100%;
-font-family: 'Poppins', sans-serif;
-font-weight: 400;
-font-size: 14px;
-line-height: 19.6px;
-color: #8C8CA1;
-margin-bottom: 10px; /* Ajuste para dar espaçamento entre os elementos */
+   /* Estilização da caixa do checkbox */
+   &:before {
+      content: "";
+      display: inline-block;
+      width: 15px; /* Mesmo tamanho que o input */
+      height: 15px; /* Mesmo tamanho que o input */
+      border: 1px solid #808080; /* Borda cinza */
+   }
 `;
-
-export const RangeContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 50px;
+export const H4Title = styled.h4`
+   font-family: "Poppins", sans-serif;
+   font-weight: 500;
+   font-size: 16px;
+   line-height: 24px;
+   color: #1a1033;
+   margin-bottom: 10px;
+   margin-left: -5px;
 `;
-
-export const Slider = styled.input`
-  position: absolute;
-  width: 100%;
-  top: 35%;
-  pointer-events: none;
-  -webkit-appearance: none;
-  background: transparent;
-
-  &::-webkit-slider-thumb {
-    pointer-events: all;
-    width: 16px;
-    height: 16px;
-    border: 2px solid #6950A1;
-    background: #FFFFFF;
-    border-radius: 50%;
-    cursor: pointer;
-    -webkit-appearance: none;
-  }
-
-  &::-moz-range-thumb {
-    pointer-events: all;
-    width: 16px;
-    height: 16px;
-    border: 2px solid #FFFFFF;
-    background: #6950A1;
-    border-radius: 50%;
-    cursor: pointer;
-  }
-`;
-
-export const SliderTrack = styled.div`
-  position: absolute;
-  height: 4px;
-  background: #ddd;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 100%;
-  border-radius: 2px;
-`;
-
-export const SliderRange = styled.div<{ left: number; width: number }>`
-  position: absolute;
-  height: 4px;
-  background: #6950A1;
-  top: 50%;
-  transform: translateY(-50%);
-  left: ${(props) => props.left}%;
-  width: ${(props) => props.width}%;
-  border-radius: 3px;
-  z-index: 2;
+export const CheckboxOption = styled.span<CheckboxOptionProps>`
+   font-size: 14px;
+   font-weight: ${(props) => (props.checked ? "700" : "400")};
+   color: "#212121";
+   margin-left: 5px;
 `;
