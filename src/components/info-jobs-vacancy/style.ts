5fed21561cc9048665e815b4c97806ab504470df
Isa Moura B. Bentes <mourabisabelle@gmail.com>
2024-05-30 23:07:20 -0300

    chore: organizando a main

--- src/components/info-jobs-vacancy/style.ts
+++ src/components/info-jobs-vacancy/style.ts
@@ -9,21 +9,30 @@ interface ResultInfoJobsDivProps {
 }
 
 export const ResultInfoJobsDiv = styled.div<ResultInfoJobsDivProps>`
-   width: 100%;
-   height: 350px;
+   width: 90%;
+   padding: 4px 3px 15px 3px;
    border-radius: 16px;
    border: 1px solid #ecf1f4;
    background-color: #ffffff;
    box-shadow: 0 15px 35px rgba(208, 208, 208, 0.2);
+   margin: 5px 15px;
    ${({ newVacancy }) =>
       newVacancy &&
       css`
-         border: 2px solid #6950a1;
+         border: 1px solid #6950a1;
          box-shadow: 0 15px 35px rgba(208, 208, 208, 0.2), 0 0 0 2px #6950a1;
       `}
 `;
 
 export const Container = styled.div`
+   display: flex;
+   justify-content: flex-start;
+   margin-top: 10px;
+   margin-right: 24px;
+   gap: 30px;
+`;
+
+export const ContainerForTitle = styled.div`
    display: flex;
    justify-content: space-between;
    margin-top: 10px;
@@ -39,8 +48,8 @@ export const ContainerTitle = styled.div`
 
 export const H1InfoJobs = styled.h1`
    margin-left: 24px;
-   font-weight: 500;
-   font-size: 20px;
+   font-weight: 600;
+   font-size: 19px;
    line-height: 24.57px;
    color: #1a1033;
 `;
@@ -55,8 +64,7 @@ export const SpanInfoJobs = styled.span<{ page?: boolean; blurred?: boolean }>`
 `;
 
 export const CardResultInfoJobs = styled.div`
-   margin-left: 20px;
-   margin-top: 24px;
+   margin: 13px 0px 8px 20px;
 `;
 
 export const InfoJobsDiv = styled.div`
@@ -71,7 +79,7 @@ export const InfoJobsDiv = styled.div`
 export const InfoJobsSpan = styled.span`
    font-family: Poppins;
    font-weight: 400;
-   font-size: 14px;
+   font-size: 12px;
    line-height: 19.6px;
    color: #4a4a68;
 `;
@@ -88,6 +96,8 @@ export const Paragraph = styled.p<{ blurred?: boolean }>`
    font-weight: 400;
    font-size: 14px;
    line-height: 25.2px;
+   margin-top: -13px;
+   margin-left: 3px;
    color: #4a4a68;
    ${(props) => props.blurred && blurredStyle}
 `;
@@ -98,11 +108,25 @@ export const StyledText = styled.span<{ page?: boolean; blurred?: boolean }>`
    font-size: 14px;
    line-height: 25.2px;
    color: #4a4a68;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   gap: 5px;
+   ${(props) => props.page && props.blurred && blurredStyle}
+`;
+
+export const InfoJobsText = styled.span<{ page?: boolean; blurred?: boolean }>`
+   font-family: Poppins, sans-serif;
+   font-weight: 400;
+   font-size: 14px;
+   line-height: 25.2px;
+   color: #4a4a68;
    ${(props) => props.page && props.blurred && blurredStyle}
 `;
 
 export const BoldText = styled.span`
    font-weight: 700;
+   color: ${({ theme }) => theme.colors.purple};
 `;
 
 export const StyledLink = styled.div`
@@ -110,9 +134,10 @@ export const StyledLink = styled.div`
    font-size: 14px;
    font-weight: 700;
    display: flex;
-   align-items: center;
-   justify-content: center;
-   margin-left: 5px;
+   align-items: flex-start;
+   justify-content: flex-start;
+   margin-top: -15px;
+   margin-left: 1px;
 
    &:hover {
       cursor: pointer;
