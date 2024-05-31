0a181ff3da331066a7eb180fd2bb2cc6d97471b5
Isa Moura B. Bentes <mourabisabelle@gmail.com>
2024-05-30 23:17:17 -0300

    chore: organizando a main

--- src/components/save-search/style.ts
+++ src/components/save-search/style.ts
@@ -1,41 +1,39 @@
 import styled from "styled-components";
 
-
-
 export const Container = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 8px;
-`
+   display: flex;
+   align-items: center;
+   gap: 8px;
+`;
 export const RegularText = styled.span`
-  font-family: Poppins, sans-serif;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 30.08px;
-  color: #FFFFFF; /* Branco */
+   font-family: Poppins, sans-serif;
+   font-weight: 400;
+   font-size: 16px;
+   line-height: 30.08px;
+   color: #ffffff; /* Branco */
 `;
 export const SpanBold = styled.span`
-font-weight: 600;
-font-size: 16px;
-line-height: 24px;
-font-style: italic;
-color: #FFFFFF; /* Branco */
-`
-export const ButtonSalved = styled.button` 
-display: flex;
-justify-content: center;
-align-items: center;
-gap: 8px;
-width: 159px;
-height: 40px;
-  background-color: #6950A1;
-  border-radius: 8px;
-  border: none;
-  cursor: pointer;
-`
+   font-weight: 600;
+   font-size: 16px;
+   line-height: 24px;
+   font-style: italic;
+   color: #ffffff; /* Branco */
+`;
+export const ButtonSalved = styled.button`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   gap: 8px;
+   width: 159px;
+   height: 40px;
+   background-color: #6950a1;
+   border-radius: 8px;
+   border: none;
+   cursor: pointer;
+`;
 export const SpanButton = styled.span`
-  font-weight: 500px;
-  font-size: 16px;
-  line-height: 24px;
-  color: #FFFFFF; /* Branco */
-`
+   font-weight: 500px;
+   font-size: 16px;
+   line-height: 24px;
+   color: #ffffff; /* Branco */
+`;
