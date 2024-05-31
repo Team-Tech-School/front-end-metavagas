f73042c08263896f1dc7136c732b3b602b1af6f7
Isa Moura B. Bentes <mourabisabelle@gmail.com>
2024-05-30 22:35:18 -0300

    Update type.ts

--- src/@types/vacancy-types/type.ts
+++ src/@types/vacancy-types/type.ts
@@ -1,3 +1,44 @@
+export interface VacancyFilters {
+   tecName?: string;
+   vacancyType?: string;
+   level?: string;
+   minSalary?: number;
+   maxSalary?: number;
+   location?: string;
+}
+
+export interface Company {
+   id: number;
+   name: string;
+   city: string;
+   state: string;
+   address: string;
+   foundedAt: string;
+   description: string;
+   createAt: string;
+   updateAt: string;
+   deleteAt: string | null;
+}
+
+export interface Advertiser {
+   id: number;
+   name: string;
+   email: string;
+   role: string;
+   createAt: string;
+   updateAt: string;
+   deleteAt: string | null;
+}
+
+export interface Technology {
+   id: number;
+   tecName: string;
+   creatorsName: string;
+   createAt: string;
+   updateAt: string;
+   deleteAt: string | null;
+}
+
 export interface Vacancy {
    id: number;
    vacancyRole: string;
@@ -6,40 +47,10 @@ export interface Vacancy {
    vacancyType: string;
    vacancyDescription: string;
    level: string;
-   companyId: number;
-   advertiserId: number;
    createAt: string;
    updateAt: string;
    deleteAt: string | null;
-   technologies: [
-      {
-         id: number;
-         tecName: string;
-         creatorsName: string;
-         createAt: string;
-         updateAt: string;
-         deleteAt: string | null;
-      }
-   ];
-   company: {
-      id: number;
-      name: string;
-      city: string;
-      state: string;
-      address: string;
-      foundedAt: string;
-      description: string;
-      createAt: string;
-      updateAt: string;
-      deleteAt: string | null;
-   };
-   advertiser: {
-      id: number;
-      name: string;
-      email: string;
-      role: string;
-      createAt: string;
-      updateAt: string;
-      deleteAt: string | null;
-   };
+   company: Company;
+   advertiser: Advertiser;
+   technologies: Technology[];
 }
