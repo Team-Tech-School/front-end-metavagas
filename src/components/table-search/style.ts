70ed679cfa377cadb04f713e9f3d3925e1a7b446
Isa Moura B. Bentes <mourabisabelle@gmail.com>
2024-05-30 23:18:10 -0300

    chore: organizando a main

--- src/components/table-search/style.ts
+++ src/components/table-search/style.ts
@@ -1,43 +1,46 @@
 import styled, { css } from "styled-components";
 
-
 export const blurredStyle = css`
-  filter: blur(5px);
+   filter: blur(5px);
 `;
+
 export const Container = styled.div<{ blurred?: boolean }>`
-position: relative;
-width: 440px;
-height: 280px;
-display: flex;
-align-items: center;
-justify-content: center;
-border-radius: 8px;
-${(props) => props.blurred && blurredStyle} // Aplicar filtro de desfoque se blurred for verdadeiro
+   position: relative;
+   width: 440px;
+   height: 280px;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   border-radius: 8px;
+   margin-right: -10px;
+   ${(props) => props.blurred && blurredStyle}
 `;
+
 export const BackgroundImage = styled.img`
-position: absolute;
-width: 100%;
-height: 100%;
-background-size: cover;
-background-position: center;
+   position: absolute;
+   width: 90%;
+   height: 90%;
+   background-size: cover;
+   background-position: center;
 `;
+
 export const Button = styled.button`
-  position: absolute;
-  background-color: #FBB04D;
-  color: #fff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 8px;
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: bold;
-  text-align: center;
+   position: absolute;
+   background-color: #fbb04d;
+   color: #fff;
+   padding: 10px 20px;
+   border: none;
+   border-radius: 8px;
+   cursor: pointer;
+   font-size: 16px;
+   font-weight: bold;
+   text-align: center;
 
-  &:hover {
-    background-color: darken(#FBB04D, 10%);
-  }
+   &:hover {
+      background-color: darken(#fbb04d, 10%);
+   }
 
-  &:active {
-    background-color: darken(#FBB04D, 20%);
-  }
+   &:active {
+      background-color: darken(#fbb04d, 20%);
+   }
 `;
